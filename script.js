06f526754f263868c479b125b0f6ddc051b594e6
WY Chan <79743156+wy-chan@users.noreply.github.com>
2022-03-27 18:50:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
+const APIurl = "https://www.metaweather.com/api/location/";
+const APIsearchName="search/?query=";///"api/location/search/?query=(query)"
+const APIsearchLL="search/?lattlong=";//"/api/location/search/?lattlong=(latt),(long)"
+
+
 class MyApp extends React.Component{
     constructor(props){
       super(props);
       this.state={
+          location:"Hong Kong",
+          todayDate:"",
+          todayText:"",
+          NavSearch: false,
+          data:{},
     };
+    this.showSearch=this.showSearch.bind(this);
+    this.closeSearch=this.closeSearch.bind(this);
+    this.searchLocationName=this.searchLocationName.bind(this);
+    this.getLocationData=this.getLocationData.bind(this);
 }
+
+componentDidMount() {
+  this.getLocationData();
+}
+
+searchLocationName(){
+  let woeid="";
+  fetch("https://www.metaweather.com/api/location/search/?query=Hong%20Kong")
+  .then((res) => res.json())
+  .then((json) => {
+      woeid=json;
+  });
+  console.log("woeid");
+  console.log(woeid);
+}
+
+
+getLocationData(){
+  this.searchLocationName();
+  
+}
+
+
+showSearch(){
+    this.setState({
+        NavSearch: true,
+    })
+}
+closeSearch(){
+    this.setState({
+        NavSearch: false,
+    })
+}
+
 render(){
+    const NavPage = (this.state.NavSearch)? 
+                    <NavSearch closeSearch={this.closeSearch}/>:
+                    <NavContent showSearch={this.showSearch}/>;
     return(
     <div id="myApp-box">
        <nav id="nav-bar">
-            <NavContent/>
+            {NavPage}
        </nav>
        <Main/>
     </div>
@@ -24,9 +75,9 @@ class NavContent extends React.Component{
         return(
          <div id="nav-bar-content">
             <div id="nav-top-btns"> 
-            <button class="search-btn nav-btn top-btn">Seach for places</button>
-            <button class="place-btn nav-btn top-btn">
-            <span class="material-icons gps-icon">
+            <button className="search-btn nav-btn top-btn" onClick={this.props.showSearch}>Seach for places</button>
+            <button className="place-btn nav-btn top-btn">
+            <span className="material-icons gps-icon">
                 gps_fixed
             </span>
             </button>
@@ -37,7 +88,7 @@ class NavContent extends React.Component{
         <div>
         <p id="nav-date">Today&nbsp;&nbsp;•&nbsp;&nbsp;Fri, 5 Jun</p>
         <p id="nav-place">
-          <span class="material-icons place-icon">
+          <span className="material-icons place-icon">
           place
          </span>
          Helsinki
@@ -55,38 +106,38 @@ class NavSearch extends React.Component{
     render(){
         return(
             <div id="nav-search-panel">
-            <button class="close-btn">
-              <span class="material-icons-outlined close-icon">
+            <button className="close-btn" onClick={this.props.closeSearch}>
+              <span className="material-icons-outlined close-icon">
               close
               </span>
             </button>
-          <form class="input-box-bar">
-          <div class="input-box">
-            <input placeholder="search location" defaultValue="Text" class="input"/>
-            <span class="material-icons search-icon">
+          <form className="input-box-bar">
+          <div className="input-box">
+            <input placeholder="search location" defaultValue="" class="input"/>
+            <span className="material-icons search-icon">
               search
               </span>
           </div>
-          <button class="search-btn search-btn-s" type="submit">Search</button>
+          <button className="search-btn search-btn-s" type="submit">Search</button>
           </form>
-          <ul class="recent-list">
+          <ul className="recent-list">
             <li>
-              <button class="nav-recent-btn">London
-              <span class="material-icons right-icon">
+              <button className="nav-recent-btn">London
+              <span className="material-icons right-icon">
                 chevron_right
               </span>
             </button>
             </li>
             <li>
-              <button class="nav-recent-btn">Barcelona
-                <span class="material-icons right-icon">
+              <button className="nav-recent-btn">Barcelona
+                <span className="material-icons right-icon">
                   chevron_right
                 </span>
               </button>
             </li>
             <li>
-              <button class="nav-recent-btn">Long Beach
-              <span class="material-icons right-icon">
+              <button className="nav-recent-btn">Long Beach
+              <span className="material-icons right-icon">
                 chevron_right
               </span>
             </button>
@@ -106,67 +157,67 @@ class Main extends React.Component{
             <main>
 
             <div id="unit-box">
-              <button class="cf-btn cf-btn-active top-btn">°C</button>
-              <button class="cf-btn top-btn">°F</button>
+              <button className="cf-btn cf-btn-active top-btn">°C</button>
+              <button className="cf-btn top-btn">°F</button>
             </div>
             <div id="forecast-box">
-              <div class="forecast-box-s">
+              <div className="forecast-box-s">
                 <h2>Tomorrow</h2>
-                <img class="week-img" src="images/Shower.png" />
-                <p class="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
+                <img className="week-img" src="images/Shower.png" />
+                <p className="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
               </div>
-              <div class="forecast-box-s">
+              <div className="forecast-box-s">
                 <h2>Tomorrow</h2>
-                <img class="week-img" src="images/Shower.png" />
-                <p class="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
+                <img className="week-img" src="images/Shower.png" />
+                <p className="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
               </div>
-              <div class="forecast-box-s">
+              <div className="forecast-box-s">
                 <h2>Tomorrow</h2>
-                <img class="week-img" src="images/Shower.png" />
-                <p class="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
+                <img className="week-img" src="images/Shower.png" />
+                <p className="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
               </div>
-              <div class="forecast-box-s">
+              <div className="forecast-box-s">
                 <h2>Tomorrow</h2>
-                <img class="week-img" src="images/Shower.png" />
-                <p class="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
+                <img className="week-img" src="images/Shower.png" />
+                <p className="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
               </div>
-              <div class="forecast-box-s">
+              <div className="forecast-box-s">
                 <h2>Tomorrow</h2>
-                <img class="week-img" src="images/Shower.png" />
-                <p class="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
+                <img className="week-img" src="images/Shower.png" />
+                <p className="week-temp"><em>16°C</em>&nbsp;&nbsp;&nbsp;11°C</p>
               </div>
             </div>
             <div id="today-box">
               <h1>Today’s Hightlights </h1>
               <div id="today-box-l">
-                <div class="today-box-s">
+                <div className="today-box-s">
                     <h2>Wind status</h2>
-                    <p class="today-info"><em>7</em>mph</p>
+                    <p className="today-info"><em>7</em>mph</p>
                     <div id="direction-box">
                       <div id="near-me-circle">
-                      <span id="near-me-icon" class="material-icons">
+                      <span id="near-me-icon" className="material-icons">
                         near_me
                       </span>
                     </div>
                         WSW
                     </div>
                 </div>
-                <div id="humid-box" class="today-box-s">
+                <div id="humid-box" className="today-box-s">
                   <h2>Humidity</h2>
-                  <p class="today-info"><em>84</em>%</p>
+                  <p className="today-info"><em>84</em>%</p>
                 <div id="humid-bar-box">
-                  <p class="humid-bar-text-box"><span>0</span><span>50</span><span>100</span></p>
+                  <p className="humid-bar-text-box"><span>0</span><span>50</span><span>100</span></p>
                   <div id="humid-bar"><div id="humid-bar-fill"></div></div>
-                  <p class="percent">%</p>
+                  <p className="percent">%</p>
                 </div>
                 </div>
-                <div class="today-box-s">
+                <div className="today-box-s">
                 <h2>Visibility</h2>
-                <p class="today-info"><em>6,4</em>miles</p>
+                <p className="today-info"><em>6,4</em>miles</p>
                 </div>
-                <div class="today-box-s">
+                <div className="today-box-s">
                   <h2>Air Pressure</h2>
-                  <p class="today-info"> <em>998</em> mb</p>
+                  <p className="today-info"> <em>998</em> mb</p>
                 </div>
               </div>
             </div>
